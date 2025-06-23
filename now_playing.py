a5c39241f1c982f6a34e228f5aec9efa9d9f34be
ihemingway <ian@tailend.ca>
2025-06-23 09:21:37 -0400

    Refactor core modules

--- now_playing.py
+++ now_playing.py
@@ -3,6 +3,7 @@ import time
 import os
 import sys
 from urllib.request import Request, urlopen
+import urllib.error
 from datetime import datetime
 from dataclasses import dataclass
 import xbmc
@@ -11,13 +12,13 @@ DISCOGS_TOKEN = "DsUoNrgSoegmaQgKqAnYbcalRpeJQArbnUxcHpQa"
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(file_path, "lib"))
-#sys.path.append("/storage/.kodi/addons/plugin.audio.cbcradio/lib")
 
 import discogs_client
 
 
-API_URL = 'https://www.cbc.ca/listen/api/v1'
+API_URL = "https://www.cbc.ca/listen/api/v1"
 LIVE_STATIONS = "/live-radio/getLiveRadioStations"
+
 
 @dataclass
 class Station:
@@ -56,192 +57,158 @@ class Track:
     cover_url: str
 
 
-def get_json_api(url):
-    #xbmc.log(level=xbmc.LOGINFO, msg=f"get_json_api({url})")
-    try:
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'}
-        req = Request(url)
-        for k in headers:
-            req.add_header(k, headers[k])
-        response = urlopen(req)
-    except urllib.error.URLError:
-        return None
-    return json.loads(response.read().decode('UTF-8'))
+class NowPlayingClient:
+    """Client for accessing CBC now playing data."""
 
-
-def get_station_names():
-    #xbmc.log(level=xbmc.LOGINFO, msg=f"called get_station_names()")
-    names = []
-    #xbmc.log(level=xbmc.LOGINFO, msg=f"{API_URL + LIVE_STATIONS}")
-    j = get_json_api(API_URL + LIVE_STATIONS)
-    stations = j['data']
-    for station in stations:
-        names.append(station['liveTitle'])
-    #xbmc.log(level=xbmc.LOGINFO, msg=f"returning get_station_names()")
-    return names
-
-
-def get_stations():
-    formatted = {}
-    j = get_json_api(API_URL + LIVE_STATIONS)
-    stations = j['data']
-    for station in stations:
-        name = station['liveTitle']
-        key = int(station['key'])
-        streams = get_streams(station)
-        station = Station(name, key, streams)
-        formatted[name] = station
-    return formatted
-
-
-def get_streams(station):
-    streams = []
-    strms = station['streams']
-    for s in strms:
-        streams.append(
-            Stream(
-                s['title'],
-                s['programGuideLocationKey'],
-                s['programGuideNetworkKey'],
-                s['streamURL'],
-                station['networkID'],
-                s['id']
-            )
+    def __init__(self):
+        self.discogs = discogs_client.Client(
+            "plugin.audio.cbcradio/2.05", user_token=DISCOGS_TOKEN
         )
-    return streams
 
-
-def get_program_schedule(key, location):
-    url = API_URL + "/program-queue/" + str(key) + "/" + location
-    program_schedule = []
-    data = get_json_api(url)['data']
-    for program in data:
-        new_image = program['programImage'].replace("${width}", "2160").replace("${ratio}", "16x9")
-        program['programImage'] = new_image
-        program_schedule.append(program)
-    #xbmc.log(level=xbmc.LOGINFO, msg=f"{program_schedule}")
-    return program_schedule
-
-
-def get_current_program(program_schedule):
-    now = time.time() * 1000
-    for program in program_schedule:
-        time_start = program['epochStart']
-        time_end = program['epochEnd']
-        #xbmc.log(level=xbmc.LOGINFO, msg=f"plugin.audio.cbcradio: title: {program['showTitle']}")
-        #xbmc.log(level=xbmc.LOGINFO, msg=f"plugin.audio.cbcradio: time_start: {time_start}")
-        #xbmc.log(level=xbmc.LOGINFO, msg=f"plugin.audio.cbcradio: time_end: {time_end}")
-        if time_start < now:
-            #xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: program started")
-            started = True
-        else:
-            started = False
-        if time_end > now:
-            ended = False
-        else:
-            #xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: program ended")
-            ended = True
-        if started and not ended:
-            show = Program(
-                program['showTitle'],
-                program['showSlugTitle'],
-                program['hostName'],
-                program['epochStart'],
-                program['epochEnd'],
-                program['programImage'],
-                program['showID'],
-                program['networkID']
-            )
-            return show
-
-
-def get_playlog(program, location):
-    now = datetime.now()
-    playlog = []
-    try:
-        playlog_url = f"https://www.cbc.ca/listen/api/v1/shows/{program.network_id}/{program.id}/playlogs/day/{now.strftime('%Y%m%d')}?withWebURL=true&locationKey={location}&xcountry=INT"
-        playlog = get_json_api(playlog_url)['data']['tracks']
-    except:
-        return playlog
-    if not playlog:
-        xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: No playlog received from CBC.")
-    return playlog
-
-
-def get_current_track(playlog):
-    now = time.time() * 1000
-    now_playing = None
-    if playlog == []:
-        xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: Empty playlog.")
-        return Track(None, None, None, None)
-    for item in playlog:
-        time_started = item['broadcastedTime']
-        if now > time_started:
-            now_playing = item
-    try:
-        from cbc import TRACK
-        if TRACK and TRACK.title == now_playing['title']:
-            now_playing["cover_url"] = TRACK.cover_url
-        else:
-            now_playing["cover_url"] = get_album_cover(now_playing['artists'], now_playing['album'])
-        return Track(
-            now_playing['title'],
-            now_playing['artists'],
-            now_playing['album'],
-            now_playing["cover_url"]
-            )
-    except TypeError:
-        return Track(None, None, None, None)
-
-
-def get_album_cover(artist, album):
-    artists = artist.split(',')
-    for art in artists:
+    @staticmethod
+    def get_json_api(url):
         try:
-            d = discogs_client.Client('plugin.audio.cbcradio/2.05', user_token=DISCOGS_TOKEN)
-            # search album/artist first. if we get a match, highly likely to be correct
-            results = d.search(album, artist=artist, type='release')
-            if results.count != 0:
-                break
-        except Exception:
+            headers = {
+                "User-Agent": (
+                    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) "
+                    "Gecko/20100101 Firefox/61.0"
+                )
+            }
+            req = Request(url)
+            for k, v in headers.items():
+                req.add_header(k, v)
+            response = urlopen(req)
+        except urllib.error.URLError:
             return None
-    try:
-        if results.count != 0:
-            xbmc.log(level=xbmc.LOGINFO, msg=f"plugin.audio.cbcradio: get_album_cover::found {results[0].images[0]['uri']}")
-            return results[0].images[0]['uri']
-    except:
-        pass
-    # try searching album only if no result from album/artist
-    #results = d.search(album, type='release')
-    #if results.count != 0:
-    #    return results[0].images[0]['uri']
-    return None # or nothing if no match at all
+        return json.loads(response.read().decode("UTF-8"))
 
+    def get_station_names(self):
+        names = []
+        data = self.get_json_api(API_URL + LIVE_STATIONS)
+        stations = data["data"]
+        for station in stations:
+            names.append(station["liveTitle"])
+        return names
 
-# this is for debugging via cli
-'''
-def run(key, location):
-    program_schedule = get_program_schedule(key, location)
-    program = get_current_program(program_schedule)
-    playlog = get_playlog(program, location)
-    track = get_current_track(playlog)
-    old_track = None
-    while 1:
+    def get_stations(self):
+        formatted = {}
+        data = self.get_json_api(API_URL + LIVE_STATIONS)["data"]
+        for station in data:
+            name = station["liveTitle"]
+            key = int(station["key"])
+            streams = self.get_streams(station)
+            formatted[name] = Station(name, key, streams)
+        return formatted
+
+    @staticmethod
+    def get_streams(station):
+        streams = []
+        for s in station["streams"]:
+            streams.append(
+                Stream(
+                    s["title"],
+                    s["programGuideLocationKey"],
+                    s["programGuideNetworkKey"],
+                    s["streamURL"],
+                    station["networkID"],
+                    s["id"],
+                )
+            )
+        return streams
+
+    def get_program_schedule(self, key, location):
+        url = f"{API_URL}/program-queue/{key}/{location}"
+        program_schedule = []
+        data = self.get_json_api(url)["data"]
+        for program in data:
+            new_image = program["programImage"].replace("${width}", "2160").replace(
+                "${ratio}", "16x9"
+            )
+            program["programImage"] = new_image
+            program_schedule.append(program)
+        return program_schedule
+
+    @staticmethod
+    def get_current_program(program_schedule):
         now = time.time() * 1000
-        if program.time_end < now:
-            time.sleep(15)
-            program = get_current_program(program_schedule)
-            track = get_current_track(playlog)
-        if track != old_track:
-            old_track = track
-            print(f"""
-Show: {program.title}
-Host: {program.host}
-Song: {track.title}
-Artist: {track.artist}
-Album: {track.album}
-==========================
-            """)
-    time.sleep(5)
-'''
+        for program in program_schedule:
+            time_start = program["epochStart"]
+            time_end = program["epochEnd"]
+            started = time_start < now
+            ended = time_end <= now
+            if started and not ended:
+                return Program(
+                    program["showTitle"],
+                    program["showSlugTitle"],
+                    program["hostName"],
+                    program["epochStart"],
+                    program["epochEnd"],
+                    program["programImage"],
+                    program["showID"],
+                    program["networkID"],
+                )
 
+    def get_playlog(self, program, location):
+        now = datetime.now()
+        playlog = []
+        try:
+            playlog_url = (
+                f"https://www.cbc.ca/listen/api/v1/shows/{program.network_id}/"
+                f"{program.id}/playlogs/day/{now.strftime('%Y%m%d')}?withWebURL=true&"
+                f"locationKey={location}&xcountry=INT"
+            )
+            playlog = self.get_json_api(playlog_url)["data"]["tracks"]
+        except Exception:
+            return playlog
+        if not playlog:
+            xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: No playlog received from CBC.")
+        return playlog
+
+    def get_current_track(self, playlog, current_track=None):
+        now = time.time() * 1000
+        now_playing = None
+        if not playlog:
+            xbmc.log(level=xbmc.LOGINFO, msg="plugin.audio.cbcradio: Empty playlog.")
+            return Track(None, None, None, None)
+        for item in playlog:
+            time_started = item["broadcastedTime"]
+            if now > time_started:
+                now_playing = item
+        try:
+            if current_track and current_track.title == now_playing["title"]:
+                now_playing["cover_url"] = current_track.cover_url
+            else:
+                now_playing["cover_url"] = self.get_album_cover(
+                    now_playing["artists"], now_playing["album"]
+                )
+            return Track(
+                now_playing["title"],
+                now_playing["artists"],
+                now_playing["album"],
+                now_playing["cover_url"],
+            )
+        except TypeError:
+            return Track(None, None, None, None)
+
+    def get_album_cover(self, artist, album):
+        artists = artist.split(",")
+        for art in artists:
+            try:
+                results = self.discogs.search(album, artist=artist, type="release")
+                if results.count != 0:
+                    break
+            except Exception:
+                return None
+        try:
+            if results.count != 0:
+                xbmc.log(
+                    level=xbmc.LOGINFO,
+                    msg=(
+                        "plugin.audio.cbcradio: get_album_cover::found "
+                        f"{results[0].images[0]['uri']}"
+                    ),
+                )
+                return results[0].images[0]["uri"]
+        except Exception:
+            pass
+        return None
